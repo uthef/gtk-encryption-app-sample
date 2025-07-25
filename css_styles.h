96ec3bf707c8dc188f1bcded2690871fbf80e011
Nightcrawler <denbrylski@gmail.com>
2025-07-25 15:38:35 +0300

    css_styles.h formatting updated

--- css_styles.h
+++ css_styles.h
@@ -3,31 +3,30 @@
 
 static const char* CLASS_DEFAULT_TEXT_VIEW = "default-text-view";
 
-static const char* CLASS_ERROR = "error";
+static const char* CLASS_ERROR = "error-label";
 static const char* CLASS_INVIS = "invis";
 static const char* CLASS_VIS = "vis";
 
-static const char* MAIN_STYLESHEET = R"(
-.default-text-view { 
-    padding: 8px; 
-    border-radius: 8px; 
-} 
-
-.error { 
-    font-weight: bold;
-    color:rgb(224, 83, 83); 
-}
-
-.invis {
-    opacity: 0;
-    transition: 0.2s;
-}
-
-.vis {
-    opacity: 1;
-    transition: 0.2s;
-}
-
-)";
+static const char* MAIN_STYLESHEET = " \
+.default-text-view { \
+    padding: 8px; \
+    border-radius: 8px; \
+} \
+\
+.error-label { \
+    font-weight: bold; \
+    color:rgb(224, 83, 83); \
+} \
+\
+.invis { \
+    opacity: 0; \
+    transition: 0.2s; \
+} \
+\
+.vis { \
+    opacity: 1; \
+    transition: 0.2s; \
+} \
+";
 
 #endif
